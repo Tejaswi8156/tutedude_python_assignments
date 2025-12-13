24cd054c536182b41f7948eeac0569ed97bdf2e2
Tejaswi Chaturvedi <164046264+Tejaswi8156@users.noreply.github.com>
2025-12-13 16:56:25 +0530

    Add files via upload
    Added Task 4 - Sum of Integers from 1 to 50.

---
+++ Task4.py
@@ -0,0 +1,10 @@
+# Task 4: Sum of Integers from 1 to 50 Using a Loop
+
+total = 0
+
+# Using a for loop to add numbers from 1 to 50
+for i in range(1, 51):
+    total += i
+
+# Displaying the final sum
+print("The sum of numbers from 1 to 50 is :", total)
