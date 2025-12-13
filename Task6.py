983ce7be2d5bfc299e9a93cbb47e4b3b8c319263
Tejaswi Chaturvedi <164046264+Tejaswi8156@users.noreply.github.com>
2025-12-13 17:14:16 +0530

    Add files via upload
    added task 6

---
+++ Task6.py
@@ -0,0 +1,16 @@
+# Task: Using the Math Module for Calculations
+
+import math
+
+# Taking input from the user
+num = float(input("Enter a Number : "))
+
+# Calculations using math module
+square_root = math.sqrt(num)
+log_value = math.log(num)
+sine_value = math.sin(num)
+
+# Displaying the results
+print("root :", square_root)
+print("logarithm :", log_value)
+print("Sine :", sine_value)
